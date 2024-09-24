459439117ffb6b73836eccf7962d9c3624457d17
thuanAlex30 <thuandh30@gmail.com>
2024-09-24 10:52:56 +0700

    login_phanquyen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from './components/auth/LoginPage';
+import UserService from './components/service/UserService';
+import ProfilePage from './components/userspage/ProfilePage';
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<LoginPage />} />
+              <Route exact path="/login" element={<LoginPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+
+              {/* Check if user is authenticated and admin before rendering admin-only routes */}
+
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/userspage/ProfilePage.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import UserService from '../service/UserService';
+import { Link } from 'react-router-dom';
+
+function ProfilePage() {
+    const [profileInfo, setProfileInfo] = useState({});
+
+    useEffect(() => {
+        fetchProfileInfo();
+    }, []);
+
+    const fetchProfileInfo = async () => {
+        try {
+            const token = localStorage.getItem('token'); // Retrieve the token from localStorage
+            const response = await UserService.getYourProfile(token);
+            setProfileInfo(response.user); // Ensure it maps to the 'user' field in your response
+        } catch (error) {
+            console.error('Error fetching profile information:', error);
+        }
+    };
+
+    return (
+        <div className="profile-page-container">
+            <h2>Profile Information</h2>
+            <p>Username: {profileInfo.username}</p>
+            <p>Email: {profileInfo.email}</p>
+            <p>Role: {profileInfo.role}</p>
+            <p>Subscription: {profileInfo.subscription_type}</p>
+            <img src={profileInfo.avatar_url} alt="Profile Avatar" />
+            {profileInfo.role === "ADMIN" && (
+                <button><Link to={`/update-user/${profileInfo.user_id}`}>Update This Profile</Link></button>
+            )}
+        </div>
+    );
+}
+
+export default ProfilePage;
